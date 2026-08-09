eddf37c22ee4c069029173355dce8b535ab2b9fb
Katta Rajashekar <rajashekarkatta18@gmail.com>
2026-08-09 14:09:51 +0530

    Solved the Factorial problem in manual and recursion

--- python_problems/basic_math_problems.py
+++ python_problems/basic_math_problems.py
@@ -66,8 +66,33 @@ class Solution:
             num += 1
 
 s = Solution()
-s.first_n_primes(10)
+s.first_n_primes(10)             # It will print first n prime numbers
 
+print("\n")
 
+# Fing the Factorial of given number
+class Solution:
+    def Factorial(self, n):
+        if n == 0 or  n == 1:
+            return 1
+        result = 1
+        for i in range(2, n+1):
+            result *= i
+        return result
+
+s = Solution()
+print(s.Factorial(6))
+
+print("\n")
+
+#Finding the Factorial of the given number using recursion
+class Solution:
+    def factorial_recursion(self, n):
+        if n == 0 or n == 1:
+            return 1
+        return n * self.factorial_recursion(n-1)
+
+s = Solution()
+print(s.factorial_recursion(5))
 
             
