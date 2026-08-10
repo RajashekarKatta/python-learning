76a5c1eab973c6aa66613a4ae90590cb43f92bde
Katta Rajashekar <rajashekarkatta18@gmail.com>
2026-08-10 23:49:27 +0530

    practiced some of the basic string problems

--- python_problems/string_problems.py
+++ python_problems/string_problems.py
@@ -49,3 +49,51 @@ class Solution:
 
 c = Solution()
 print(c.second_non_repeated_char("rajashekar"))
+
+# Check if Two Strings are Anagrams
+
+class Solution:
+    def check_anagrams(self, s1, s2):
+        if len(s1) != len(s2):
+            return False
+        freq = {}
+        for char in s1:
+            freq[char] = freq.get(char, 0) + 1
+
+        for char in s1:
+            if char not in freq:
+                return False
+            freq[char] -= 1
+
+            if freq[char] < 0:
+                return False
+        return True
+
+c = Solution()
+print(c.check_anagrams("Silent", "listen"))
+
+
+# Capitalize First Letter of Each Word (Title Case) in the fiven sentence
+class Solution:
+    def first_letter_capital_in_each_word(self, text):
+        words = text.split()
+        result = []
+        for word in words:
+            result.append(word.capitalize())
+        return ' '.join(result)
+
+c = Solution()
+print(c.first_letter_capital_in_each_word("hello eveery one"))
+
+# Replace All Digits with *
+class Solution:
+    def replace(self, text):
+        result = ''
+        for char in text:
+            if char.isdigit():
+                result += '*'
+            else:
+                result += char
+        return result
+c = Solution()
+print(c.replace("a1b2c3"))
